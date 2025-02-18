1fe2473752ced56a7ec4a3a90822605c12c3bc33
Amy Benner <amybenner@iMac.local>
2025-02-17 16:14:44 -0800

    Populated store.js and added the store to main.jsx.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./app/App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
